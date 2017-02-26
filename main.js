35000f034efcd624e9d7ce3c5a18cf133a7c16a5
BobbyHo <angela.bobby@gmail.com>
2017-02-26 23:25:03 +0800

    Wasay It's much better now!

--- main.js
+++ main.js
@@ -1,64 +1,111 @@
 #! /usr/bin/env node
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const crx = require('unzip-crx');
 const archiver = require('archiver');
 const path = require('path');
 
 var crxFilePath = process.argv[2];
-var crxUnpackPath = process.argv[3] || path.basename(crxFilePath, '.crx') + '_unzipped';
+var crxTempUnpackPath = __dirname + '/temp';
 
-var log = function(type = 'status', content) {
-    if (type == 'error') {
-        console.error(`Error: ${content}`)
-    } else {
-        console.log(`Status: ${content}`)
-    }
+var log = (type = 'status', content) => {
+	if (type == 'error') {
+		console.error(`Error: ${content}`);
+	} else {
+		console.log(`Status: ${content}`);
+	}
+};
+
+var pushObject = (obj, key, value) => {
+	obj[key] = value;
+	return obj;
+};
+
+var readCrxFile = () => {
+	if (path.extname(crxFilePath) == '.crx') {
+		crx(crxFilePath, crxTempUnpackPath)
+			.then(() => {
+				log('status', 'Unpacking CRX file...')
+				modifyManifest();
+			})
+			.catch((err) => {
+				log('error', 'Failed to unpack CRX file');
+				throw err;
+			})
+	} else {
+		log('error', `\"${crxFilePath}\" is not a valid Chrome extension`);
+		terminate();
+	}
 }
 
-var pushObject = function(obj, key, value) {
-    obj[key] = value;
-    return obj;
+var modifyManifest = () => {
+	fs.readFile(`${crxTempUnpackPath}/manifest.json`, 'utf8', (err, data) => {
+		if (err) {
+			log('error', 'Failed to read manifest.json from unpacked crx')
+			throw err;
+		} else {
+			var manifestContent = JSON.parse(data);
+
+			var declareGeckoSupport = {
+				'gecko': {
+					'id': `${manifestContent.name}@crx-to-xpi`
+				}
+			};
+			// Infos that need to be pushed into "manifest.json"
+
+			manifestContent = pushObject(manifestContent, 'applications', declareGeckoSupport);
+
+			log('status', 'Trying to write modified manifests into manifest.json')
+			fs.writeFile(`${crxTempUnpackPath}/manifest.json`, JSON.stringify(manifestContent), 'utf8', (err) => {
+				if (err) {
+					log('error', 'Failed to write manifests');
+					throw err;
+				} else {
+					log('status', 'Manifests were written successfully')
+					packXPI(manifestContent.name);
+				}
+			});
+			// Write modified manifest
+		}
+	});
 }
 
-var readCrxFile = function() {
-    if (path.extname(crxFilePath) == '.crx') {
-        crx(crxFilePath, crxUnpackPath)
-            .then(() => {
-                log('status', 'Unpacking CRX file.')
-                modifyManifest()
-            })
-    } else {
-        log('error', `\"${crxFilePath}\" is not a valid Chrome extension`);
-    }
+var packXPI = (name) => {
+	var xpiStream = fs.createWriteStream(__dirname + `/${name}.xpi`);
+	var xpi = archiver('zip', {
+		store: true
+	});
+
+	xpiStream.on('close', function () {
+		log('status', 'XPI was packed and generated successfully');
+		log('status', `XPI is located in \"${__dirname}/${name}.xpi\"`)
+		cleanupTempData()
+	});
+	xpi.on('error', function (err) {
+		log('error', 'Failed to pack XPI from source code');
+		terminate()
+	});
+
+	xpi.pipe(xpiStream);
+	xpi.directory(crxTempUnpackPath, '/')
+	xpi.finalize();
+
+	log('status', 'Trying to pack XPI from source code')
 }
 
-var modifyManifest = function() {
-    fs.readFile(`${crxUnpackPath}/manifest.json`, 'utf8', (err, data) => {
-        var manifestContent = JSON.parse(data);
-        
-        var declareGeckoSupport = {
-            'gecko': {
-                'id': `${manifestContent.name}@crx-to-xpi`
-            }
-        };
-        // Infos that need to be pushed into "manifest.json"
-        
-        manifestContent = pushObject(manifestContent, 'applications', declareGeckoSupport);
-        fs.writeFile(`${crxUnpackPath}/manifest.json`, JSON.stringify(manifestContent), 'utf8', (err) => {
-            packXPI(manifestContent.name)
-        });
-        // Write modified manifest
-    })
+var cleanupTempData = () => {
+	fs.remove(crxTempUnpackPath, (err) => {
+		if (err) {
+			log('error', 'Failed to remove temp directory');
+			throw err;
+		} else {
+			terminate();
+		}
+	});
 }
 
-var packXPI = function(name) {
-    var output = fs.createWriteStream(__dirname + `/${name}.xpi`);
-    var xpi = archiver('zip', {store: true});
-    xpi.pipe(output);
-    xpi.directory(`${__dirname}/${crxUnpackPath}/`, '/')
-    xpi.finalize();
+var terminate = () => {
+	log('status', 'Tasks finished. Terminating... Good Bye!');
 }
 
 readCrxFile();
-// log('tatus', 'Tasks finished. Terminating......');
